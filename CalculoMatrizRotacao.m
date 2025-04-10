913ded773aafa69794c27647d1d0449daeae275b
PHPGomes <pedrohpgomes@gmail.com>
2025-04-10 10:00:05 -0300

    .

---
+++ CalculoMatrizRotacao.m
@@ -0,0 +1,18 @@
+%% CALCULO MATRIZ ROTAÇÃO %%
+
+%% CLEAR
+clear
+clc
+
+%% VALOR ANGULO
+teta = 0;
+
+%% CALCULO SIN E COS
+sin = sind(teta);
+cos = cosd(teta);
+
+%% CRIAÇÃO MATRIZ
+
+Rx = [1 0 0 ; 0 cos -sin ; 0 sin cos]
+Ry = [cos 0 sin ; 0 1 0 ; -sin 0 cos]
+Rz = [cos -sin 0 ; sin cos 0 ; 0 0 1]
