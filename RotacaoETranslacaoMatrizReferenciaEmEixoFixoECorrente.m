86c1ae54947f92ece9a7c22592fa515eb6113a3f
PHPGomes <pedrohpgomes@gmail.com>
2025-04-14 18:07:44 -0300

    .

--- RotacaoETranslacaoMatrizReferenciaEmEixoFixoECorrente.m
+++ RotacaoETranslacaoMatrizReferenciaEmEixoFixoECorrente.m
@@ -19,9 +19,9 @@ Ry = [cos 0 sin 0;0 1 0 0;-sin 0 cos 0;0 0 0 1];
 Rz = [cos -sin 0 0;sin cos 0 0; 0 0 1 0;0 0 0 1];
 
 % Paramentro de deslocamento para Matriz Translação
-qx = 3;
+qx = 0;
 qy = 5;
-qz = 0;
+qz = 3;
 
 % Criação Matriz Translação
 Trans = [1 0 0 qx;0 1 0 qy; 0 0 1 qz; 0 0 0 1];
@@ -29,8 +29,13 @@ Trans = [1 0 0 qx;0 1 0 qy; 0 0 1 qz; 0 0 0 1];
 % Matriz Referencia
 B = [0 1 0 1; 1 0 0 1;0 0 -1 1; 0 0 0 1];
 
+% ponto ref
+p = [1;5;4;1];
+T = [qx;qy;qz;1];
+
 % Eq de movimento (Fixo antes e Corrente depois)
 R = Ry*Rz*B*Trans*Rx;
+P = ((Rz*Rx*p) + Rz*Rx*T)-[0;0;0;1]; % Para Pontos não se pode multiplicar a translação, com isso tem que rotacionar e somar
 
 % Matriz Resultante
 disp(R)
