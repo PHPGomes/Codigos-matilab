0774ce417cd7456e27f746db91e570145d8d8d93
PHPGomes <pedrohpgomes@gmail.com>
2025-04-15 13:56:41 -0300

    .

--- Cinematica Inversa/Orientacao/CinematicaInversaOrientacao.m
+++ Cinematica Inversa/Orientacao/CinematicaInversaOrientacao.m
@@ -14,14 +14,16 @@ R = [0 -1 0 214.20;0 0 1 589.62;-1 0 0 1100;0 0 0 1];
 %% Parametros da Matriz
 
 
-% Cálculo dos ângulos de Euler (convenção ZYX) 
-beta  = atan2d(-R(3,1), sqrt(R(1,1)^2 + R(2,1)^2));  % pitch
-alpha = atan2d(R(3,2), R(3,3));                     % roll
-gamma = atan2d(R(2,1), R(1,1));                     % yaw
+% Cálculo dos ângulos de Euler (convenção ZYZ) 
+phiO  = atan2d(-R(3,1), sqrt(R(1,1)^2 + R(2,1)^2));  % pitch
+phiN = atan2d(R(2,1), R(1,1));                     % roll
+phiA = atan2d((-R(2,3)*cosd(phiN)+R(1,3)*sind(phiN)), (R(2,2)*cosd(phiN)-R(1,2)*sind(phiN)));     % yaw
 
 
-fprintf('Yaw (γ)   = %.2f°\n', gamma);
-fprintf('Pitch (β) = %.2f°\n', beta);
-fprintf('Roll (α)  = %.2f°\n', alpha);
+fprintf('phiA = %.2f°\n', phiA);
+fprintf('phiN = %.2f°\n', phiN);
+fprintf('phiO = %.2f°\n', phiO);
+    
+
 
 
