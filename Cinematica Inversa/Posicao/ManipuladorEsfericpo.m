11a79fa70a69502a00aca453685b0b2effd5e59c
PHPGomes <pedrohpgomes@gmail.com>
2025-04-14 22:52:56 -0300

    .

---
+++ Cinematica Inversa/Posicao/ManipuladorEsfericpo.m
@@ -0,0 +1,10 @@
+%% Cinemática Inversa Manipulador Esférico
+
+%% Clear
+clear
+clc
+
+%% Matriz Esférica
+T = [1 0 0 1;0 1 0 2;0 0 1 3;0 0 0 1]; %Colocar a Matriz dada aqui
+
+%% Parametros da Matriz
