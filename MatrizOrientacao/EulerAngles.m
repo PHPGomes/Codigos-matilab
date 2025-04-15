f42dd44101ab124d5e64886244a756ca10d833d4
PHPGomes <pedrohpgomes@gmail.com>
2025-04-14 21:02:19 -0300

    .

--- MatrizOrientacao/EulerAngles.m
+++ MatrizOrientacao/EulerAngles.m
@@ -7,7 +7,7 @@
 clear
 clc
 
-%% Parametros
+%% Parametros (3x3)
 ax = 0; % Ângulo de rotação em X
 ay = 0; % Ângulo de rotação em Y
 az = 0; % Ângulo de rotação em Z
@@ -24,6 +24,29 @@ sz = sind(az);
 Rx = [1 0 0;0 cx -sx;0 sx cx];
 Ry = [cy 0 sy;0 1 0;-sy 0 cy];
 Rz = [cz -sz 0;sz cz 0;0 0 1];
+
+%% Parametros (4x4) rot+pos
+ax = 0; % Ângulo de rotação em X
+ay = 0; % Ângulo de rotação em Y
+az = 0; % Ângulo de rotação em Z
+
+cx = cosd(ax);
+sx = sind(ax);
+
+cy = cosd(ay);
+sy = sind(ay);
+
+cz = cosd(az);
+sz = sind(az);
+
+qx = 0;
+qy = 0;
+qz = 0;
+w = 1;
+
+Rx = [1 0 0 qx;0 cx -sx qy;0 sx cx qz;0 0 0 w];
+Ry = [cy 0 sy qx;0 1 0 qy;-sy 0 cy qz; 0 0 0 w];
+Rz = [cz -sz 0 qx;sz cz 0 qy;0 0 1 qz; 0 0 0 w];
 
 %% Equação da Orientação
 % OBS: Exemplo comum: Ordem ZYX, Primeiro você rotaciona em torno de X (Roll, φ), Depois em torno de Y (Pitch, θ) 
