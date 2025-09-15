57dec61f3e3fb637230b9930534450639ad8d890
Pedro Henrique Pigozzi Gomes <112020010+PHPGomes@users.noreply.github.com>
2025-09-15 18:05:33 -0300

    .

--- Robotica/2025-2/Cinematica Inversa/Posicao/Geral.m
+++ Robotica/2025-2/Cinematica Inversa/Posicao/Geral.m
@@ -3,7 +3,7 @@ clear
 clc
 
 %% Matriz
-M = [1 0 0 1; 0 1 0 5;0 0 1 4;0 0 0 1];
+M = [0 1 0 1;1 0 0 1;0 0 -1 1;0 0 0 1]
 
-%% Ex Operações (Matriz M recebeu uma rotação z de 30 depois umas y de 60)
-T = Rz(90)*Rx(90)*M*Trans(0,0,3)*Trans(0,5,0)
+%% Operações (Colocar a matriz M primeiro e ir adicionando os movimentos nas extremidades, fixos antes e correntes depois)
+T = M
