5a8f061e0b73ed63810ca9f63bddf7b39b8bfa40
PHPGomes <pedrohpgomes@gmail.com>
2025-09-24 09:16:16 -0300

    .

--- Robotica/2025-2/Cinematica Direta/Orientacao/CinematicaDiretaOrientacao.m
+++ Robotica/2025-2/Cinematica Direta/Orientacao/CinematicaDiretaOrientacao.m
@@ -9,7 +9,7 @@ clc
 
 %% Matriz Matriz de rotação (ou extraída de matriz homogênea)
 
-R = [0.527 -0.574 0.628 4; 0.369 0.819 0.439 6; -0.766 0 0.643 9; 0 0 0 1]
+R = [0.5 0.86 0 4.3;0.61 -0.35 -0.7 -1.76;-0.61 0.35 -0.7 3.76; 0 0 0 1]
 
 %% Parametros da Matriz
 
